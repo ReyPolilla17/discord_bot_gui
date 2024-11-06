1f20ae775188c510594bbf02052ca0a7485609b0
Polilla <naway.luisju@gmail.com>
2024-11-06 08:30:02 -0600

    created a base electron project, next commits will be mainly for GUI design

---
+++ index.js
@@ -0,0 +1,26 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+const CreateWindow = () => {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, "preload.js")
+        }
+    });
+
+    win.loadFile("index.html");
+}
+
+app.whenReady().then(() => {
+    CreateWindow();
+});
+
+app.on("activate", () => {
+    if(BrowserWindow.getAllWindows().length === 0) CreateWindow();
+});
+
+app.on("window-all-closed", () => {
+    if(process.platform !== "darwin") app.quit();
+});
